fdf955a9755c105435635c26e7c97b028797f031
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2022-03-14 21:32:25 -0700

    bot: updated nixpkgs on 2022-03-15

--- pkgs.nix
+++ pkgs.nix
@@ -1,10 +1,11 @@
+
 { system ? builtins.currentSystem }:
 
 import (builtins.fetchTarball {
   # Descriptive name to make the store path easier to identify
-  name = "nixos-unstable-2022-03-12";
+  name = "nixpkgs-unstable-2022-03-14";
   # Commit hash for nixos-unstable as of 2018-09-12
-  url = "https://github.com/nixos/nixpkgs/archive/fcd48a5a0693f016a5c370460d0c2a8243b882dc.tar.gz";
+  url = "https://github.com/nixos/nixpkgs/archive/3239fd2b8f728106491154b44625662e10259af2.tar.gz";
   # Hash obtained using `nix-prefetch-url --unpack <url>`
-  sha256 = "06nyvac1azpa9gqdhj9py6ljsvbfgx49ilp8kf6w0w9clxba64vg";
+  sha256 = "0bn0dcd7casv9kwxpa22mxizs8gmrpgi5xba0qr7g6fi7l5lnp1v";
 }) { system = system; }
