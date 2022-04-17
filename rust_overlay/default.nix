820367fefdf57da60c26fc675e8ca664c97e52f9
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2022-04-17 05:29:12 -0700

    bot: updated nixpkgs on 2022-04-17

--- rust_overlay/default.nix
+++ rust_overlay/default.nix
@@ -1,7 +1,7 @@
 import (builtins.fetchTarball {
   # Descriptive name to make the store path easier to identify
-  name = "rust-overlay-2022-04-16";
-  url = "https://github.com/oxalica/rust-overlay/archive/d154de7dae2da8b52084ddbfe95ce6e5b0ba0a37.tar.gz";
+  name = "rust-overlay-2022-04-17";
+  url = "https://github.com/oxalica/rust-overlay/archive/26b570500cdd7a359526524e9abad341891122a6.tar.gz";
   # Hash obtained using `nix-prefetch-url --unpack <url>`
-  sha256 = "0wpfwa64i1h3ga0jafpfh8kqr1nbbkj01r6f920z9fjfwn3xw08y";
+  sha256 = "0rr9kcl6pns5yspjc59hg5ksbddyy8439395n5mnqh6dvsacnvbv";
 })
