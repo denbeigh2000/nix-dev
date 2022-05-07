add3855d0ae67a9f72a7f687f901e1d90d482771
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2022-05-07 09:16:26 -0700

    bot: updated nixpkgs on 2022-05-07

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,7 +1,7 @@
 import (builtins.fetchTarball {
   # Descriptive name to make the store path easier to identify
-  name = "nixpkgs-unstable-2022-04-16";
-  url = "https://github.com/nixos/nixpkgs/archive/faad370edcb37162401be50d45526f52bb16a713.tar.gz";
+  name = "nixpkgs-unstable-2022-05-07";
+  url = "https://github.com/nixos/nixpkgs/archive/2fdb6f2e08e7989b03a2a1aa8538d99e3eeea881.tar.gz";
   # Hash obtained using `nix-prefetch-url --unpack <url>`
-  sha256 = "1d82d4vh0layf6n925j0h2nym16jbvcvps3l5m8ln9hxn0m6gadn";
+  sha256 = "12wfjn35j9k28jgp8ihg96c90lqnplfm5r2v5y02pbics58lcrbw";
 })
--- rust_overlay/default.nix
+++ rust_overlay/default.nix
@@ -1,7 +1,7 @@
 import (builtins.fetchTarball {
   # Descriptive name to make the store path easier to identify
-  name = "rust-overlay-2022-04-17";
-  url = "https://github.com/oxalica/rust-overlay/archive/26b570500cdd7a359526524e9abad341891122a6.tar.gz";
+  name = "rust-overlay-2022-05-07";
+  url = "https://github.com/oxalica/rust-overlay/archive/43f4c4319fd29d07912a65d405ff03069c7748c4.tar.gz";
   # Hash obtained using `nix-prefetch-url --unpack <url>`
-  sha256 = "0rr9kcl6pns5yspjc59hg5ksbddyy8439395n5mnqh6dvsacnvbv";
+  sha256 = "0301x86j0m49c4sf22c9fiyigsn9a4k4bhwmaxvw6yiz2qnqyg0k";
 })
